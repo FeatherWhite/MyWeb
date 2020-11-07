357a0cd416a76f99a34795760d9bf0903d54d017
FeatherWhite <tomw5823@outlook.com>
2020-11-07 10:15:59 +0800

    webpack练习:添加demo/main.js

--- JS学习/Vuejs/demo/webpack.config.js
+++ JS学习/Vuejs/demo/webpack.config.js
@@ -1,5 +1,8 @@
 var config = {
-
+    "scripts": {
+        "test": "echo \"Error: no test specified\" && exit 1",
+        "dev": "webpack-dev-server --port 8958 --open --config webpack.config.js"
+      },
 };
 
 //相当于export default config
